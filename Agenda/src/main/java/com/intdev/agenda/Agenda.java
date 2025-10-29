79bd112a7d2b3a7371a298f4b80a02da3e278ac3
Adrián González Blanco <z3u2thnder@proton.me>
2025-10-29 17:57:35 +0100

    initial Commit

---
+++ Agenda/src/main/java/com/intdev/agenda/Agenda.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.intdev.agenda;
+
+/**
+ *
+ * @author adrian.gonzalez
+ */
+public class Agenda {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
